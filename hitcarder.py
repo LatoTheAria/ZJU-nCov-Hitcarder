a2c3cf1a729f3221cfd8fb4e04562c47f3f48b72
Archer Chen <81161141+LatoTheAria@users.noreply.github.com>
2022-04-11 01:10:58 +0800

    Update hitcarder.py

--- hitcarder.py
+++ hitcarder.py
@@ -87,12 +87,12 @@ class HitCarder(object):
         except IndexError as _:
             raise RegexMatchError('Relative info not found in html with regex')
 
-        with open("form.txt", "r", encoding="utf-8") as f:
-            if new_form == f.read():
-                return True
-        with open("form.txt", "w", encoding="utf-8") as f:
-            f.write(new_form)
-        return False
+        #with open("form.txt", "r", encoding="utf-8") as f:
+        #    if new_form == f.read():
+        #        return True
+        #with open("form.txt", "w", encoding="utf-8") as f:
+        #    f.write(new_form)
+        return True
 
     def get_info(self, html=None):
         """Get hit card info, which is the old info with updated new time."""
